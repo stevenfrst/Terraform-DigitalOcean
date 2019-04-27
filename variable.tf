7882f634fdc78de5091eca9ec0158f81673a67da
Steven Humam <oppaidaisuki363@gmail.com>
2019-04-27 10:01:07 +0700

    Create variable.tf
    upload

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "image" {
+  default = "ubuntu-16-04-x64"
+}
+
+variable "name" {
+  default = "instance"
+}
+
+variable "region" {
+  default = "sgp"
+}
+
+variable "size" {
+  default = "512mb"
+}
